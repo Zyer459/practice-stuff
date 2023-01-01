2ef83e2bd4a52a75d4cba395906035235d121144
Zyer459 <asiful.sumit@gmail.com>
2023-01-01 21:22:51 +0600

    useEffect hook with 1 parameter & 0 dependency

--- counter-app/src/index.js
+++ counter-app/src/index.js
@@ -5,9 +5,29 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
 import Counter from './components/counter';
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
 
+/* useEffect allows us to perform various side effects in components
+such as fetch data, directly update DOM, timers
+useEffect takes 2 arguments, 2nd on is optional
+useEffect(<function>, <dependency>)*/
 
+function Timer () {
+  const [count, setCount] = useState(0);
 
+  useEffect(() => {
+    setTimeout(() => {
+      setCount(count => count+1);
+    }, 1000)
+  });
+  return (
+    <h1>I've rendered count {count} times</h1>
+  )
+}
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render();
+root.render(<Timer />);
+
+/* but the timer keeps counting even though it should count only once
+its because useEffect runs on every render
+which means everytime the count changes, a render happens, which triggers another effect
+to control this use the 2nd parameter <dependency>*/
