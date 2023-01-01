59f6e0a042f105f5f7e42c37df380a182e96cb71
Zyer459 <asiful.sumit@gmail.com>
2023-01-01 22:18:36 +0600

    renders if variable changes, used 2 parameters

--- counter-app/src/index.js
+++ counter-app/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
-import Counter from './components/counter';
+//import Counter from './components/counter';
 import { useState,useEffect } from 'react';
 
 /* useEffect allows us to perform various side effects in components
@@ -12,22 +12,36 @@ such as fetch data, directly update DOM, timers
 useEffect takes 2 arguments, 2nd on is optional
 useEffect(<function>, <dependency>)*/
 
-function Timer () {
-  const [count, setCount] = useState(0);
+/*useState & useEffect are functions*/
 
-  useEffect(() => {
-    setTimeout(() => {
-      setCount(count => count+1);
-    }, 1000)
-  }, []);// <-- add empty array as dependency parameter to auto render only once 
+/* the code below uses only 1 dependency
+if there are multiple dependencies they should be included in the useEffect dependency array*/
+
+function Counter () {
+  const [count, setCount] = useState(0);// <-- set value of count variable to 0 && setCount updates count value everytime it is called
+  const [calculation, setCalculation] = useState(0);// <-- set value of calculation variable to 0 && setCalculation updates calculation value
+
+  useEffect (() => {
+    setCalculation (() => count * 2);
+  }, [count]) //<-- add count variable here as dependency parameter(2nd parameter) of useEffect hook && renders when count value changes
+
   return (
-    <h1>I've rendered count {count} times</h1>
+    <>
+    <h1>Count: {count}</h1>
+    <h1>Calculation: {calculation}</h1>
+    <button
+    type="button"
+    onClick={() => setCount((c) => c + 1)}
+    class="btn btn-dark">
+    +
+    </button>
+    </>
   )
 }
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Timer />);
+root.render(<Counter />);
 
-/* but the timer keeps counting even though it should count only once
-its because useEffect runs on every render
-which means everytime the count changes, a render happens, which triggers another effect
-to control this use the 2nd parameter <dependency>*/
+/* in the above code onClick att. calls an arrow function which calls the setCount which then updates the count variable 
+and that triggers the setCalculation to render variables*/
+
+//there may be some mistakes I made here, I'm new to this :p
