d181ff49dfe3fb2ff9c5bb4b79651e25bf836e97
Zyer459 <asiful.sumit@gmail.com>
2023-01-01 17:46:06 +0600

    use state hook

--- counter-app/src/index.js
+++ counter-app/src/index.js
@@ -4,15 +4,22 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
+import Counter from './components/counter';
+import { useState } from 'react';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function FavouriteColor () {
+  const [color, setColor] = useState("red");
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  return (
+    <>
+    <h1>My favourite color is {color}!</h1>
+    <button
+    type="button"
+    onClick={() => setColor("blue")}>Blue
+    </button>
+    </>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<FavouriteColor />);
