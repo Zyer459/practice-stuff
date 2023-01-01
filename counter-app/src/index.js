940f86b0daf79b7b4ec84fe950623317fd07aa85
Zyer459 <asiful.sumit@gmail.com>
2023-01-01 20:38:36 +0600

    clear all for UseEffect hook

--- counter-app/src/index.js
+++ counter-app/src/index.js
@@ -7,46 +7,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 import Counter from './components/counter';
 import { useState } from 'react';
 
-function FavouriteColor () {
-  const [car, setColor] = useState({
-    brand: "Ford",
-    model: "Mustang",
-    year: 1964,
-    color: "color ?"
-  });
 
-  const UpdateColor = (a) => {
-    setColor((previousState) => {
-      return {...previousState, color: a.target.innerHTML}
-    });
-  }
-
-  return (
-    <>
-    <h1>My favourite is a {car.color} {car.brand}, {car.model} </h1>
-    <button
-    type="button"
-    onClick={(a) => UpdateColor(a)}
-    class="btn btn-outline-primary">
-    Blue
-    </button>
-
-    <button
-    type="button"
-    onClick={(a) => UpdateColor(a)}
-    class="btn btn-outline-success">
-      Green
-    </button>
-
-    <button
-    type="button"
-    onClick={(a) => UpdateColor(a)}
-    class="btn btn-outline-danger">
-      Red
-    </button>
-    </>
-  )
-}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<FavouriteColor />);
+root.render();
