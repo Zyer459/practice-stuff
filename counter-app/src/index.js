8165e5526f17260f252b500144a0247b75714c01
Zyer459 <asiful.sumit@gmail.com>
2023-01-02 18:44:37 +0600

    the solution to the problem with useContext hook

--- counter-app/src/index.js
+++ counter-app/src/index.js
@@ -1,11 +1,10 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
 //import Counter from './components/counter';
-import { useState,useEffect } from 'react';
+import { useState,createContext,useContext } from 'react'; // <-- import createContext & useContext
 
 /* ReactContext is a way to manage state globally
 it can be used with useState hook to share state between deeply nested components more easily than useState alone */
@@ -14,47 +13,59 @@ it can be used with useState hook to share state between deeply nested component
 To illustrate below are many nested components, the component at the top & bottom of the stack needs access to the state.
 To do this without Context, need to pass the state as "props" through each nested component this is called "prop drilling"*/
 
-//The problem >
+/** the solution: 
+*1) create context 
+*2) use context provider property to wrap the tree of components that need the state Context
+*3) to use the Context in a child component, we need to access it using the useContext hook
+*4) include the useContext in the import statement 
+*5) then access  the user Context in all components*/
+
+//The solution >
+
+const UserContext = createContext(); // <-- initialize it & assign it to a variable
 
 function Component1 () {
   const [user, setUser] = useState("John Doe");
 
+  // Wrap child components in the Context Provider and supply the state value >
   return (
-    <>
-    <h1>Hello {user}</h1>
-    <Component2 user={user} />
-    </>
+    <UserContext.Provider value={user}>
+      <h1>Hello {user}!</h1>
+      <Component2 />
+    </UserContext.Provider>
   );
 }
+// now all components in this tree will have access to user Context
 
-function Component2 ({ user }) {
+function Component2 () {
   return (
     <>
     <h2>Component 2</h2>
-    <Component3 user={user} />
+    <Component3 />
     </>
   );
 }
 
-function Component3 ({ user }) {
+function Component3 () {
   return (
     <>
     <h3>Component 3</h3>
-    <Component4 user={user}/>
+    <Component4/>
     </>
   );
 }
 
-function Component4 ({ user }) {
+function Component4 () {
   return (
   <>
   <h4>Component 4</h4>
-  <Component5 user={user}/>
+  <Component5/>
   </>
   );
 }
 
-function Component5 ({ user }) {
+function Component5 () {
+  const user = useContext(UserContext)
   return (
   <>
   <h1>Component 5</h1>
@@ -63,14 +74,10 @@ function Component5 ({ user }) {
   );
 }
 
-/** in the above example problem the last component (Component5) requires access to the state {user}.
-But Components 2-4  does not require access to state {user}.
-But still has to pass the state from one component to the next until it reaches last component,
-so that Component5 can use the state. */
-
-/** this is a tedious task to the next Component with the state as prop & attribute until it reaches its destination.
- * What if there were 50 or 500 components!? Will you write the same code again & again? 
- * This greatly increases the probability for error.
+/**in the solution above the { user } parameter and attribute is removed from components 2-4 
+ * and in component 5 assign the useContext function with UserContext (which contains the createContext()) as parameter
+ * assigned to a variable
+ * then use that variable
  */
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
