433a70d4bfab1b3de60f400533f37c0dc343d2d4
Zyer459 <asiful.sumit@gmail.com>
2023-01-03 23:44:58 +0600

    an example problem for useCallback hook

--- counter-app/src/index.js
+++ counter-app/src/index.js
@@ -5,6 +5,7 @@ import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
 //import Counter from './components/counter';
 import { useState, useEffect, useRef } from 'react';
+import Todos from "./Todos";
 
 /** the React useCallback hook returns a memoized callback function
  * think memoization as = caching a value so that it doesn't need to be recalculatd.
@@ -16,8 +17,34 @@ import { useState, useEffect, useRef } from 'react';
  * useMemo returns a (memoized value) & useCallback returns a (memoized function)
  */
 
+/**PROBLEM:
+ * one reason for using useCallback hook is to prevent a component from re-rendering unless its props have changed
+ */
+
+const App = () => {
+  const [count, setCount] = useState(0);
+  const [todos, setTodos] = useState([]);
+
+  const increment = () => {
+    setCount((c) => c + 1);
+  };
+  const addTodo = () => {
+    setTodos((t) => [...t], "New Todo");
+  };
+
+  return (
+    <>
+    <Todos todos={todos} addTodo ={addTodo} />
+    <hr />
+    <div>
+      Count: {count}
+      <button onClick={increment}> + </button>
+    </div>
+    </>
+  );
+};
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render (<Todos />)
+root.render (<App />)
 
 /**
 */
