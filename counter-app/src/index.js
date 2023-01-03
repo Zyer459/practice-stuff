7e395794c8a05b57a64f842aaaac0d662d87dd97
Zyer459 <asiful.sumit@gmail.com>
2023-01-03 20:43:48 +0600

    some info about useCallback hook

--- counter-app/src/index.js
+++ counter-app/src/index.js
@@ -6,7 +6,14 @@ import 'bootstrap/dist/css/bootstrap.css';
 //import Counter from './components/counter';
 import { useState, useEffect, useRef } from 'react';
 
-/**
+/** the React useCallback hook returns a memoized callback function
+ * think memoization as = caching a value so that it doesn't need to be recalculatd.
+ * this allows us to isolate resource intensive functions so that, they wiil not automatically run on every render.
+ * the useCallback only runs when one of its dependencies update.
+ * this can improve performance.
+ * 
+ * the useCallback & useMemo hooks are similar. Main difference is their return funcions
+ * useMemo returns a (memoized value) & useCallback returns a (memoized function)
  */
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
