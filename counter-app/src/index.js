94ac60c0e95cffc01ab9fda1a92eb43314c79573
Zyer459 <asiful.sumit@gmail.com>
2023-01-03 20:00:02 +0600

    some info about useReducer hook

--- counter-app/src/index.js
+++ counter-app/src/index.js
@@ -6,6 +6,17 @@ import 'bootstrap/dist/css/bootstrap.css';
 //import Counter from './components/counter';
 import { useState, useEffect, useRef } from 'react';
 
+/**the useReducer hook is similar to the useState hook
+ * allows for custom state logic
+ * if its required to keep track of multiple pieces of state that require complex logic,
+ * useReducer hook may be helpful
+ * useReducer hook accepts 2 arguments > (reducer), (initialState).
+ * Syntax: useReducer (<reducer>, <initialState>).
+ * the <reducer> function contains custom state logic &
+ * <initialState> will be a simple value but generelly contain an {object}
+ * the useReducer hook returns the currrent [State] & a [Dispatch] method.
+ */
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render (<App />)
 
