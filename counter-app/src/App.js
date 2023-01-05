7f3785f328a5a9f6667054b8644e698c84f29787
Zyer459 <asiful.sumit@gmail.com>
2023-01-05 14:26:51 +0600

    created TodoList & updated App

--- counter-app/src/App.js
+++ counter-app/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { ReactDOM } from 'react';
 import './App.css';
+import TodoList from './TodoList';
 
 function App() {
   return (
-    <h1>Hello world</h1>
+    <TodoList />
   );
 }
 
---
+++ counter-app/src/TodoList.jsx
@@ -0,0 +1,11 @@
+//new file for practice
+//could use a .js instead of .jsx, but using jsx is a bit better
+import React from "react";
+
+function TodoList () {
+    return (
+        <h1>Hello world</h1>
+    )
+}
+
+export default TodoList;
