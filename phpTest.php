cbbf59dbe4b80d8d861db727928112bc8ae7daaa
Zyer459 <asiful.sumit@gmail.com>
2023-01-05 19:53:27 +0600

    started php

---
+++ phpTest.php
@@ -0,0 +1,20 @@
+<!-- This is the start of PHP learning
+php is a server-side scripting language.
+
+Q. What is a php file? - -> php file contains text, html, css, JS & php code
+and its executed on the server.
+
+Q. What can php do? - -> 1. can generate dynamic page content
+2. can create, open, read, write, delete $ close files
+3. can collect form data
+4. can send/receive cookies
+5. can add, delete & modify data in a database
+6. can be used to control user access
+7. can encrypt data
+
+With PHP you are not limited to output HTML. It can output images or PDF or XHTML/XML files
+
+Q Why use php? - -> available on various platforms, compatible with almost all servers today & easy and efficient
+
+Q. How to install php> - -> download required zip from php.net > extract in a new folder > set path from 
+"edit system environment variable" > the path should be the folder's path which contains the php files you just extracted
