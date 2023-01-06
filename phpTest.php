39fc824a2f5fad5f4b627c374053b4660a5f6032
Zyer459 <asiful.sumit@gmail.com>
2023-01-06 19:19:17 +0600

    PHP echo and print Statements

--- phpTest.php
+++ phpTest.php
@@ -1,4 +1,12 @@
-<!--  -->
+<!-- with php there are 2 basic ways to get outputs echo & print
+they are almost the same, both are used for output 
+only few small differences: 
+    
+    echo has no return value and, 
+    print has a return value of 1 & can be used in expressions.
+    echo can take multiple parameters (arguments), while print takes only 1 argument.
+    echo is marginally faster than print
+-->
 
 <!DOCTYPE html>
 <html lang="en">
