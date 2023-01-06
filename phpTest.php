efcc76362a56a4732c6b243e2d494049297b7ee7
Zyer459 <asiful.sumit@gmail.com>
2023-01-06 19:13:34 +0600

    clear

--- phpTest.php
+++ phpTest.php
@@ -1,5 +1,4 @@
-<!-- php variables: 
-in php the variable starts with $ sign followed by name of the variable-->
+<!--  -->
 
 <!DOCTYPE html>
 <html lang="en">
@@ -14,28 +13,6 @@ in php the variable starts with $ sign followed by name of the variable-->
 </body>
 
 <?php
-
-/** The static keyword 
- * normally after a function is completed/executed, all of its variables are deleted.
- * However, we might want to keep the local variables for further use
- * to do this use the static keyword when you 1st declare a local variable
- */
-
- function myTest () {
-    static $x = 0;
-    echo ($x);
-    $x++;
- }
- myTest();
- echo "<br>"; // write HTML code as strings
- myTest();
- echo "<br>"; // this creates a new line break
- myTest();
- echo "<br>";
-
- /**here everytime the function is called, the variable will keep its previous value from the last function
-  * to test it remove the static keyword from the variable to see its effect
-  */
 ?>
 </html>
 
