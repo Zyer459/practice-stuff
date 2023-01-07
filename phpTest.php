2125ac0e40a074e903ed5a3b423877b1c5fe1939
Zyer459 <asiful.sumit@gmail.com>
2023-01-07 23:51:59 +0600

    php numbers-Integers

--- phpTest.php
+++ phpTest.php
@@ -1,14 +1,18 @@
-<!--  PHP STRINGS:
-      some commonly used functions to manipulate strings in php
+<!--  PHP NUMBERS: 
+      php provides automatic data type conversion.
+      if you assign an integer to a variable, the type of the variable wiil automatically be integer.
+      if you again assign a string to that variable it will be a string data type.
+      this automatic conversion although useful, it can sometimes break the code.
 
-      1) strlen(): takes a string or variable with string as value & returns the length of string.
-      2) str_word_count(): returns the number of WORDS (caution: not letters!) in a string.
-      3) strrev(): reverses a string.
-      4) strpos(): searches for a specific text in a string of texts & 
-      returns the position of the 1st letter of the 1st occurrence of the required text.
-      5) str_replace(): replaces some characters with some other characters in a string. Takes 3 arguments (to be replaced, replace with, string)
+      php has the following predefined constants for integers:
+        PHP_INT_MAX - The largest integer supported
+        PHP_INT_MIN - The smallest integer supported
+        PHP_INT_SIZE - The size of an integer in bytes (memory allocation of the integer)
 
-
+      php has the following functions to check if the type of a variable is integer or not:
+        is_int()
+        is_integer() -> alias of is_int()
+        is_long() -> alias of is_int()
 -->
 
 <!DOCTYPE html>
@@ -24,20 +28,14 @@
 </body>
 
 <?php
-/**EXAMPLES:  */
-echo strlen ("Hello world!@#"); //counts alpha-numeric puncuation & special characters
-echo "</br>";
-echo "</br>";
-echo str_word_count("Hello world!"); //outputs 2
-echo "</br>";
-echo "</br>";
-echo strrev("Hello, world!");
-echo "</br>";
-echo "</br>";
-echo strpos("Hello, world", "ld");//outputs 10
-echo "</br>";
-echo "</br>";
-echo str_replace("world", "Dolly", "Hello world!"); // outputs Hello Dolly! instead of Hello world!
+/**EXAMPLE: */
+
+$x = 5596;
+var_dump(is_int($x));// returns true
+echo("</br>");
+
+$x = 55.96;
+var_dump(is_int($x));// returns false
 ?>
 </html>
 
