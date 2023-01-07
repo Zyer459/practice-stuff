d2f15791a004435e83cd70084fb60d2bb084d442
Zyer459 <asiful.sumit@gmail.com>
2023-01-08 00:13:46 +0600

    php numbers-infinity

--- phpTest.php
+++ phpTest.php
@@ -4,18 +4,16 @@
       if you again assign a string to that variable it will be a string data type.
       this automatic conversion although useful, it can sometimes break the code.
 
-      php has the following predefined constants for floats (from PHP 7.2):
-        PHP_FLOAT_MAX - The largest representable floating point number
-        PHP_FLOAT_MIN - The smallest representable positive floating point number
-        PHP_FLOAT_MAX - The smallest representable negative floating point number
-        PHP_FLOAT_DIG - The number of decimal digits that can be rounded into a float and back without precision loss
-        PHP_FLOAT_EPSILON - The smallest representable positive number x, so that x + 1.0 != 1.0
+      A numeric value larger than PHP_FLOAT_MAX is considered infinite.
+        
         
 
-      php has the following functions to check if the type of a variable is float or not:
-        is_float()
-        is_double() -> alias of is_float()
-        
+      php has the following functions to check if a numeric value is infinite or not:
+        is_finite()
+        is_infinite()
+
+        however, 
+        the php var_dump() returns the data type & value      
 -->
 
 <!DOCTYPE html>
@@ -33,16 +31,16 @@
 <?php
 /**EXAMPLE: */
 
-/*$x = 5596;
-var_dump(is_int($x));// returns true
-echo("</br>");*/
+$x = 1.9e411;
+var_dump($x);// (INF) means its an infinite number
 
-$x = 55.96;
-var_dump(is_int($x));// returns false-> because its a float see below
 echo("</br>");
 
-var_dump(is_float($x));// returns true
+var_dump(is_finite($x));// returns false
 
+echo("</br>");
+
+var_dump(is_infinite($x));// returns true
 
 ?>
 </html>
