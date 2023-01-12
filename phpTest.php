7a8045cd257e7d6cb69653ea28c4d078b1d1d77d
Zyer459 <asiful.sumit@gmail.com>
2023-01-12 20:17:39 +0600

    php array constants

--- phpTest.php
+++ phpTest.php
@@ -8,12 +8,11 @@
             Unlike variables, constants are automatically global across the entire script.
 
             create a constant:
-            use the define() function
-            syntax -> define(name, value, case-insensitive)
-            name -> specifies the name of the constant (the name must be a string)
-            value -> specifies the value of constant (can be any data type)
-            case-insensitive -> specifies whether the constant name should be case-insensitive, default is false (it means it is case-sensitive)
-            enter boolean values for 3rd parameter
+            use the define() function -> php 7
+            or 
+            const keyword
+
+            create an array constant using define() function in php 7
 -->
 
 <!DOCTYPE html>
@@ -29,26 +28,21 @@
 </body>
 
 <?php
-/**EXAMPLE: Create a constant with a case-sensitive name:*/
+/**EXAMPLE: create an array constant*/
 
-define ("num", 55); // 3rd parameter is set to default which is false, if type echo (nuM) or anything else it throws fatal error
-echo (num); // returns the value of num which is 55
+define("cars", ["Alfa Romeo","BMW","Toyota"]); // create array const using define()
+echo cars[1]; // returns BMW
+
 echo "<br>";
 
-define("value", true); // warning
-echo value; // returns 1 for true & 0 for false
-echo "<br>";
-
-define("GREET", "hello world", true);
-echo gREET; // should return the string hello world
-echo "<br>";
+const cars2 = ["Ford","BMW","Toyota"]; // create array const using const keyword
+echo cars2[0];
 ?>
 </html>
 
-<!-- very important
-    In versions prior to PHP 8.0, it was possible to define case-insensitive constants by passing true as third parameter of the function.
-    In PHP 7.3 and 7.4, the ability to define case-insensitive constants is deprecated.
-    Since PHP 8.0, defining case-insensitive constants has been removed. (Lindevs)
+<!-- 
+    note: see the official documentation on constants -> https://www.php.net/manual/en/language.constants.php
+    can use the const keyword to create not just array constants
 -->
 
 <!-- 
