f366eb6cf861ef49b9add8863c8fe9fcde318c5a
Zyer459 <asiful.sumit@gmail.com>
2023-01-13 22:44:28 +0600

    php functions-p1

--- phpTest.php
+++ phpTest.php
@@ -1,5 +1,16 @@
 <!--  PHP FUNCTIONS: 
-                    
+                    user defined function syntax: 
+
+                    function functionName () {
+                        some code;
+                    }
+
+                    it looks same as javascript in php functionNames are not case-sensitive but its good to keep it as case-sensitive
+                    php is loosely typed language so php automatically associates data type to variables, depending on its value
+                    you can do things like adding a string to an integer without causing an error.
+
+                    In PHP 7, type declarations were added. This gives us an option to specify the expected data type when declaring a function,
+                    and by adding the strict declaration, it will throw a "Fatal Error" if the data type mismatches.
 -->
 
 <!DOCTYPE html>
@@ -15,8 +26,14 @@
 </body>
 
 <?php
-/**EXAMPLE: */
+/**EXAMPLE: In the following example we try to send both a number and a string to the function without using strict & add types declarations to variables:*/
 
+function addNumbers (int $a, int $b) {
+    return $a + $b;
+}
+
+echo addNumbers(5, "5 days");
+// since strict is NOT enabled "5 days" is changed to int(5), and it will return 10
 ?>  
 </html>
 
