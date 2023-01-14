49b5a8df57e2c26a1e7bd5834c342e39bc8772c4
Zyer459 <asiful.sumit@gmail.com>
2023-01-14 17:27:11 +0600

    php associative arrays

--- phpTest.php
+++ phpTest.php
@@ -1,9 +1,16 @@
 <!--  PHP ARRAYS:
                 in php arrays are created using the array() function
                 there are 3 types of arrays in php these are :
-                Indexed arrays - Arrays with a numeric index
-                Associative arrays - Arrays with named keys
-                Multidimensional arrays - Arrays containing 1 or more arrays
+
+                Associative arrays - Arrays with named keys: the arrays that use named keys assigned to them are called associative arrays
+                there are 2 ways to create an associative array
+
+                1) $age = array ("Name1" => "25", "Name2" => "35", "Name3" => "22"); the numbers here are strings
+
+                2) $age["Name1"] = "25";
+                   $age["Name2"] = "35";
+                   $age["Name3"] = "22";
+
 
 -->
 
@@ -20,19 +27,35 @@
 </body>
 
 <?php
-/**EXAMPLE: */
+/**EXAMPLE: using the 1st method*/
 
-$cars = array ("Audi", "BMW", "Ford");
-$cars[2] = "Toyota";
-echo "I like $cars[0], $cars[1] and $cars[2].";
+$age = array ("Peter" => 25, "John" => 35, "Jake" => 22); // I changed the numbers from strings to integer data type
+
+echo "Jake is " .$age["Jake"]. " years old";
 echo "<br>";
 
-echo "thats a total of ".count($cars). " cars";
+/**Looping through an associative array: use foreach loop*/
+$age2["A"] = "25"; // kept the numbers as strings
+$age2["B"] = "35";
+$age2["C"] = "22";
+
+foreach ($age2 as $i => $i_value) {
+    echo "Key = " .$i. ", value = " .$i_value;
+    echo "<br>";
+}
 
 ?>  
 </html>
 
 <!-- 
+    using arrays in strings need the period ( . )
+    syntax of foreach() loop: 
+
+        foreach ($array as $value) {
+            code to be executed;
+        }
+
+        works only on arrays, used to loop through each key/value pair in associative array
 -->
 
 <!-- 
