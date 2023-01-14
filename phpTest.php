b1695e267f266e5389893b11364e8553567d013a
Zyer459 <asiful.sumit@gmail.com>
2023-01-14 19:17:32 +0600

    php super globals-$globals

--- phpTest.php
+++ phpTest.php
@@ -1,14 +1,19 @@
-<!--  PHP ARRAYS:
-                in php arrays are created using the array() function
-                there are 3 types of arrays in php these are :
+<!--  PHP GLOBAL VARIABLES-SUPER GLOBAL:
+                SUPER GLOBALS are per-defined php varibles that are always available in all scopes
+                they are as follows:
+                $GLOBALS
+                $_SERVER
+                $_REQUEST
+                $_POST
+                $_GET
+                $_FILES
+                $_ENV
+                $_COOKIE
+                $_SESSION
 
-                Sorting arrays: 
-                sort() - sort arrays in ascending order  (the order may be alphabetical or numerical it depends on the data)
-                rsort() - sort arrays in descending order  (the order may be alphabetical or numerical it depends on the data)
-                asort() - sort associative arrays in ascending order, according to the VALUE  (the order may be alphabetical or numerical it depends on the data)
-                ksort() - sort associative arrays in ascending order, according to the KEY  (the order may be alphabetical or numerical it depends on the data)
-                arsort() - sort associative arrays in descending order, according to the VALUE  (the order may be alphabetical or numerical it depends on the data)
-                krsort() - sort associative arrays in descending order, according to the KEY  (the order may be alphabetical or numerical it depends on the data)
+                PHP $GLOBALS: $GLOBALS is a PHP super global variable which is used to access global variables from anywhere in the PHP script 
+                (also from within functions or methods).
+                PHP stores all global variables in an array called $GLOBALS[index]. The index holds the name of the variable.
 
 -->
 
@@ -25,12 +30,23 @@
 </body>
 
 <?php
-/**EXAMPLE: */
+/**EXAMPLE: below is an example of $GLOBALS*/
+
+$x = 75;
+$y = 25;
+ 
+function addition() {
+  $GLOBALS['z'] = $GLOBALS['x'] + $GLOBALS['y'];// the names are in string
+}
+ 
+addition();
+echo $z;
 
 ?>  
 </html>
 
 <!-- 
+    In the example above, since z is a variable present within the $GLOBALS array, it is also accessible from outside the function!
 -->
 
 <!-- 
