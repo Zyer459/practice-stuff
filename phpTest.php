be7c9c63e564a485e03940b277a90af5e92de206
Zyer459 <asiful.sumit@gmail.com>
2023-01-14 17:02:43 +0600

    php arrays intro

--- phpTest.php
+++ phpTest.php
@@ -1,20 +1,9 @@
-<!--  PHP FUNCTIONS: 
-                Passing Arguments by Reference:
-                 there are 2 ways of passing arguments to a function
-                 1) pass by value 2)pass by reference
-
-                 1) pass by value: its the usual way of passing arguments to a function,
-                 it means that a copy of the value is used in the function & the variable (which had the value) passed
-                 into the function cannot be changed by the function -> in short
-                 the value of the variable is copied & saved in a different variable inside the function & the new variable can only be changed within the function
-                 and it will not affect the variable outside the function
-
-                 2) pass by reference: When a function argument is passed by reference,
-                 changes to the argument also change the variable that was passed into the function.
-                 To turn a function argument into a reference, the & operator is used.
-
-                 in short -> changed a variable outside a function by passing it as an argument by reference
-                 changes made to the argument within the function will affect the variable outside
+<!--  PHP ARRAYS:
+                in php arrays are created using the array() function
+                there are 3 types of arrays in php these are :
+                Indexed arrays - Arrays with a numeric index
+                Associative arrays - Arrays with named keys
+                Multidimensional arrays - Arrays containing 1 or more arrays
 
 -->
 
@@ -31,15 +20,10 @@
 </body>
 
 <?php
-/**EXAMPLE: Use a pass-by-reference argument to update a variable*/
+/**EXAMPLE: */
 
-function add_five(&$value) {
-    $value += 5;
-  }
-  
-  $num = 2;
-  add_five($num);
-  echo $num;
+$cars = array ("Audi", "BMW", "Ford");
+echo "I like $cars[0], $cars[1] and $cars[2].";
 
 ?>  
 </html>
