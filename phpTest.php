7892a358148cbd0fb5814d78c8cad8538670637f
Zyer459 <asiful.sumit@gmail.com>
2023-01-15 21:16:18 +0600

    php form validation

--- phpTest.php
+++ phpTest.php
@@ -1,7 +1,4 @@
-<!--  PHP FORMS:
-
-
-
+<!--  PHP FORM VALIDATION:
 -->
 
 <!DOCTYPE html>
@@ -13,54 +10,31 @@
     <title>php document</title>
 </head>
 <body>
-    <form action="phpTest.php" method="post">
-      <label>
-        Name: <input type="text" name="Name"/>
-      </label>
-      <label>
-        E-mail: <input type="email" name="Email"/>
-      </label>
-      <input type="submit"/>
-    </form>
-
-    <?php
-    /**EXAMPLE: basic php form handling */
-
-    /*$Name = $_POST["Name"];
-    $Email = $_POST["Email"];
-
-    echo "Welcome $Name";
-    echo "<br>";
-    echo "Your e-mail is $Email";
-    commented this because of a better soln look below
-    */
-    ?>
-
-    <h1>Welcome</h1> <?php echo $_POST["Name"];?>
-    <br>
-    <h1>Your e-mail is: </h1> <?php echo $_POST["Email"];?>
+  <h1>PHP Form Validation Example:</h1>
+  <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"])?>" method="post">
+    Name: <input type="text" name="Name"/>
+    E-mail: <input type="email" name="Email"/>
+    Website: <input type="text" name="WebSite"/>
+    Comment: <textarea rows="3" cols="17" name="Comment"></textarea>
+    Gender: 
+    <input type="radio" name="Gender" value="Male">Male
+    <input type="radio" name="Gender" value="Female">Female
+  </form>
 </body>
 
 </html>
 
+<!-- What is the $_SERVER["PHP_SELF"] variable?
+The $_SERVER["PHP_SELF"] is a super global variable that returns the filename of the currently executing script.
+So, the $_SERVER["PHP_SELF"] sends the submitted form data to the page itself, instead of jumping to a different page.
+This way, the user will get error messages on the same page as the form.
+-->
+
+<!-- What is the htmlspecialchars() function?
+The htmlspecialchars() function converts special characters to HTML entities.
+This means that it will replace HTML characters like < and > with &lt; and &gt;
+ This prevents attackers from exploiting the code by injecting HTML or Javascript code (Cross-site Scripting attacks) in forms.
+-->
+
 <!-- 
-  The code above is quite simple. However, the most important thing is missing. You need to validate form data to protect your script from malicious code.
-  This page does not contain any form validation, it just shows how you can send and retrieve form data.
-  Proper validation of form data is important to protect your form from hackers and spammers!
--->
-
-<!-- GET
-Information sent from a form with the GET method is visible to everyone (all variable names and values are displayed in the URL).
-GET also has limits on the amount of information to send. The limitation is about 2000 characters.
-However, because the variables are displayed in the URL, it is possible to bookmark the page. This can be useful in some cases.
-GET may be used for sending non-sensitive data.
-Note: GET should NEVER be used for sending passwords or other sensitive information!
--->
-
-<!-- POST
-Information sent from a form with the POST method is invisible to others (all names/values are embedded within the body of the HTTP request)
-and has no limits on the amount of information to send.
-Moreover POST supports advanced functionality such as support for multi-part binary input while uploading files to server.
-However, because the variables are not displayed in the URL, it is not possible to bookmark the page.
-Developers prefer POST for sending form data.
 -->
