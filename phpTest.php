ee79741fc61917c91abd9b4f9ab9ec9357c58fed
Zyer459 <asiful.sumit@gmail.com>
2023-01-16 22:35:13 +0600

    used loop but bug

--- phpTest.php
+++ phpTest.php
@@ -8,24 +8,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>php document</title>
+    <style>.required {color: red;}</style>
 </head>
 <body>
-  <h1>PHP complete form example</h1>
-  <p>* required field</p>
-  <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
-    Name: <input type="text" name="Name"/>
-    E-mail: <input type="text" name="Email"/>
-    Website: <input type="text" name="Website"/>
-    Comment: <textarea name="Comment" rows="3" cols="30"></textarea>
-    Gender: 
-    <input type="radio" name="Gender" value="Male"/> Male
-    <input type="radio" name="Gender" value="Female"/> Female
-    <input type="submit"/>
-  </form>
-  <h1>Your input:</h1>
-
-  <?php
+<?php
   $name = $email = $website = $comment = $gender = "";
+  $nameErr = $emailErr = $websiteErr = $commentErr = $genderErr = "";
 
   $name = $_POST["Name"];
   $email = $_POST["Email"];
@@ -34,6 +22,7 @@
   $gender = $_POST["Gender"];
   
   $container = array ($name, $email, $website, $comment, $gender);
+  $Errormsgs = array ($nameErr, $emailErr, $genderErr);
 
   function test ($data) {
     $data = trim((string) $data);
@@ -43,14 +32,37 @@
   }
 
   for ($i = 0; $i < 5; $i++) {
-    echo test ($container[$i]);
-    echo "<br>";
+    if (empty($container[$i])) {
+      $Errormsgs[$i] = "This field is required";
+    }
+    else {
+      test ($container[$i]);
+    }
   }
   
+?>
+
+  <h1>PHP complete form example</h1>
+  <p class="required">* required field</p>
+  <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
+    Name: <input type="text" name="Name"/> <span class="required">* <?php echo $nameErr;?></span>
+    E-mail: <input type="text" name="Email"/> <span class="required">* <?php echo $emailErr;?></span>
+    Website: <input type="text" name="Website"/>
+    Comment: <textarea name="Comment" rows="3" cols="30"></textarea>
+    Gender: 
+    <input type="radio" name="Gender" value="Male"/> Male
+    <input type="radio" name="Gender" value="Female"/> Female <span class="required">* <?php echo $genderErr;?></span>
+    <input type="submit"/>
+  </form>
+  <h1>Your input:</h1>
+  <?php
+  $container = array ($name, $email, $website, $comment, $gender);
+  for ($i = 0; $i < 5; $i++) {
+    echo ($container[$i]);
+  } 
   ?>
 </body>
 </html>
-
 <!-- 
 -->
 
