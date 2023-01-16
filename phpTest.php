db0d515adc9e8ad05944a0a403663e3b1959bb58
Zyer459 <asiful.sumit@gmail.com>
2023-01-16 21:21:45 +0600

    PHP html complete form

--- phpTest.php
+++ phpTest.php
@@ -1,4 +1,4 @@
-<!--  PHP FORM VALIDATION:
+<!--  PHP FORM COMPLETE:
 -->
 
 <!DOCTYPE html>
@@ -10,34 +10,27 @@
     <title>php document</title>
 </head>
 <body>
-  <h1>PHP Form Validation Example:</h1>
-  <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"])?>" method="post">
+  <h1>PHP complete form example</h1>
+  <p>* required field</p>
+  <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
     Name: <input type="text" name="Name"/>
-    E-mail: <input type="email" name="Email"/>
-    Website: <input type="text" name="WebSite"/>
-    Comment: <textarea rows="3" cols="17" name="Comment"></textarea>
+    E-mail: <input type="text" name="Email"/>
+    Website: <input type="text" name="Website"/>
+    Comment: <textarea name="Comment" rows="3" cols="30"></textarea>
     Gender: 
-    <input type="radio" name="Gender" value="Male">Male
-    <input type="radio" name="Gender" value="Female">Female
+    <input type="radio" name="Gender" value="Male"/> Male
+    <input type="radio" name="Gender" value="Female"/> Female
+    <input type="submit"/>
   </form>
+  <h1>Your input:</h1>
 </body>
-
 </html>
 
-<!-- What is the $_SERVER["PHP_SELF"] variable?
-The $_SERVER["PHP_SELF"] is a super global variable that returns the filename of the currently executing script.
-So, the $_SERVER["PHP_SELF"] sends the submitted form data to the page itself, instead of jumping to a different page.
-This way, the user will get error messages on the same page as the form.
+<!-- 
 -->
 
-<!-- What is the htmlspecialchars() function?
-The htmlspecialchars() function converts special characters to HTML entities.
-This means that it will replace HTML characters like < and > with &lt; and &gt;
- This prevents attackers from exploiting the code by injecting HTML or Javascript code (Cross-site Scripting attacks) in forms.
+<!-- 
 -->
 
-<!-- Note on PHP Form Security
-The $_SERVER["PHP_SELF"] variable can be used by hackers!
-If PHP_SELF is used in your page then a user can enter a slash (/) and then some Cross Site Scripting (XSS) commands to execute.
-
+<!-- 
 -->
