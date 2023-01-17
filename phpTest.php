8957abf5b4dd8d6b28c143c5d5841f8ec5298c8c
Zyer459 <asiful.sumit@gmail.com>
2023-01-17 21:53:42 +0600

    php include-example-1

--- phpTest.php
+++ phpTest.php
@@ -1,7 +1,24 @@
-<!--  PHP ADVANCED: PHP date & time
-the date() function is used to format date and or time
-the function also formats a timestamp to a more readable date and time
-syntax: data(format, timestamp);
+<!--  
+PHP ADVANCED: PHP include or require
+    The include (or require) statement takes all the text/code/markup that exists in the specified file and copies it into the file that uses the include statement.
+    Including files is very useful when you want to include the same PHP, HTML, or text on multiple pages of a website.
+    syntax: include 'filename'; or require 'filename';
+-->
+
+<!-- 
+PHP include and require Statements: It is possible to insert the content of one PHP file into another PHP file (before the server executes it), 
+    with the include or require statement.
+
+    The include and require statements are identical, except upon failure:
+    require will produce a fatal error (E_COMPILE_ERROR) and stop the script from executing
+    include will only produce a warning (E_WARNING) and the script will continue
+
+    So, if you want the execution to go on and show users the output, even if the include file is missing, use the include statement.
+    Otherwise, always use the require statement to include a key file to the flow of execution.
+    This will help avoid compromising your application's security and integrity, just in-case one key file is accidentally missing.
+
+    Including files saves a lot of work. This means that you can create a standard header, footer, or menu file for all your web pages.
+    Then, when the header needs to be updated, you can only update the header include file.
 -->
 
 <!DOCTYPE html>
@@ -13,25 +30,21 @@ syntax: data(format, timestamp);
     <title>php document</title>
 </head>
 <body>
-    <?php
-    date_default_timezone_set("Asia/Dhaka"); //set the time zone to get correct time below
-    echo "the time is: " .date("h:i:sa");
-    ?>
-    <br>
-    <br>
-    <?php
-    // time until december 1st
-    //$d2 gives time in seconds so its divided to get days, again divide by 30 to get months
-    $d1 = strtotime("Dec 1");
-    $d2 = ceil (($d1-time())/60/60/24);
-    echo $d2;
-    ?>
+    <!-- example-1: assume that we have a standard footer to include in multiple html files-->
+    <h1>Welcome to my home page!</h1>
+    <p>Some text.</p>
+    <p>Some more text.</p>
+    <?php include 'footer.php';?> <!-- here the footer file doesn't really exist its just for example -->
 </body>
 </html>
 <!-- 
+    since the file doesn't exist the include statement returns a warning
 -->
 
-<!-- 
+<!-- below is the content of the footer .php file
+<php
+echo "<p>Copyright &copy; 1999-" . date("Y") . " W3Schools.com</p>";
+>
 -->
 
 <!-- 
