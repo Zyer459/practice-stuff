933e3a5d59499b5698f1c80c71bcbb441037faf4
Zyer459 <asiful.sumit@gmail.com>
2023-01-17 14:09:42 +0600

    no loop=no bugs=more code

--- phpTest.php
+++ phpTest.php
@@ -11,35 +11,50 @@
     <style>.required {color: red;}</style>
 </head>
 <body>
+  
 <?php
-  $name = $email = $website = $comment = $gender = "";
-  $nameErr = $emailErr = $websiteErr = $commentErr = $genderErr = "";
+// define variables and set to empty values
+$nameErr = $emailErr = $genderErr = $websiteErr = "";
+$name = $email = $gender = $comment = $website = "";
 
-  $name = $_POST["Name"];
-  $email = $_POST["Email"];
-  $website = $_POST["Website"];
-  $comment = $_POST["Comment"];
-  $gender = $_POST["Gender"];
-  
-  $container = array ($name, $email, $website, $comment, $gender);
-  $Errormsgs = array ($nameErr, $emailErr, $genderErr);
-
-  function test ($data) {
-    $data = trim((string) $data);
-    $data = stripslashes($data);
-    $data = htmlspecialchars($data);
-    return $data;
-  }
-
-  for ($i = 0; $i < 5; $i++) {
-    if (empty($container[$i])) {
-      $Errormsgs[$i] = "This field is required";
-    }
-    else {
-      test ($container[$i]);
-    }
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  if (empty($_POST["name"])) {
+    $nameErr = "Name is required";
+  } else {
+    $name = test_input($_POST["name"]);
   }
   
+  if (empty($_POST["email"])) {
+    $emailErr = "Email is required";
+  } else {
+    $email = test_input($_POST["email"]);
+  }
+    
+  if (empty($_POST["website"])) {
+    $website = "";
+  } else {
+    $website = test_input($_POST["website"]);
+  }
+
+  if (empty($_POST["comment"])) {
+    $comment = "";
+  } else {
+    $comment = test_input($_POST["comment"]);
+  }
+
+  if (empty($_POST["gender"])) {
+    $genderErr = "Gender is required";
+  } else {
+    $gender = test_input($_POST["gender"]);
+  }
+}
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
 ?>
 
   <h1>PHP complete form example</h1>
@@ -47,7 +62,7 @@
   <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
     Name: <input type="text" name="Name"/> <span class="required">* <?php echo $nameErr;?></span>
     E-mail: <input type="text" name="Email"/> <span class="required">* <?php echo $emailErr;?></span>
-    Website: <input type="text" name="Website"/>
+    Website: <input type="text" name="Website"/> <span class="required"> <?php echo $websiteErr;?></span>
     Comment: <textarea name="Comment" rows="3" cols="30"></textarea>
     Gender: 
     <input type="radio" name="Gender" value="Male"/> Male
@@ -55,12 +70,7 @@
     <input type="submit"/>
   </form>
   <h1>Your input:</h1>
-  <?php
-  $container = array ($name, $email, $website, $comment, $gender);
-  for ($i = 0; $i < 5; $i++) {
-    echo ($container[$i]);
-  } 
-  ?>
+
 </body>
 </html>
 <!-- 
