2089b2c65447b1a468e517d9d736195d46a8ae1e
Zyer459 <asiful.sumit@gmail.com>
2023-01-18 20:16:46 +0600

    php open/read

--- phpTest.php
+++ phpTest.php
@@ -5,6 +5,9 @@ PHP ADVANCED: PHP file handling
             Common errors are: editing the wrong file, filling a hard-drive with garbage data, 
             and deleting the content of a file by accident.
             The readfile() function reads a file and writes it to the output buffer.
+
+             better method to open files is with the fopen() function.
+             This function gives you more options than the readfile() function.
 -->
 
 <!-- 
@@ -29,17 +32,38 @@ content of the file:
 </head>
 <body>
 <?php
-/**The PHP code to read the file and write it to the output buffer is as follows
-* (the readfile() function returns the number of bytes read on success): */
-echo readfile("webdictionary.txt");
+/**example also generates a message if the fopen() function is unable to open the specified file:*/
+$myFile = fopen ("webdictionary.txt", "r") or die ("Unable to open file");
+echo fread($myFile,filesize("webdictionary.txt"));
+fclose($myFile);
 ?>
 </body>
 </html>
 <!-- 
-    The readfile() function is useful if all you want to do is open up a file and read its contents.
+The first parameter of fopen() contains the name of the file to be opened
+  and the second parameter specifies in which mode the file should be opened.
 -->
 
 <!-- 
+  Modes: 
+        r = Open a file for read only. File pointer starts at the beginning of the file
+        w = Open a file for write only.
+            Erases the contents of the file or creates a new file if it doesn't exist. 
+            File pointer starts at the beginning of the file
+        a = Open a file for write only.
+            The existing data in file is preserved.
+            File pointer starts at the end of the file.
+            Creates a new file if the file doesn't exist
+        x = Creates a new file for write only. Returns FALSE and an error if file already exists
+        r+ = Open a file for read/write. File pointer starts at the beginning of the file
+        w+ = Open a file for read/write.
+             Erases the contents of the file or creates a new file if it doesn't exist.
+             File pointer starts at the beginning of the file
+        a+ = Open a file for read/write.
+             The existing data in file is preserved.
+             File pointer starts at the end of the file.
+             Creates a new file if the file doesn't exist
+        x+ = Creates a new file for read/write. Returns FALSE and an error if file already exists
 -->
 
 <!-- 
