43cb60561c704c24c22a323435266c1b7c6c52c1
Zyer459 <asiful.sumit@gmail.com>
2023-01-18 20:55:57 +0600

    php create/write

--- phpTest.php
+++ phpTest.php
@@ -11,15 +11,25 @@ PHP ADVANCED: PHP file handling
 -->
 
 <!-- 
-content of the file:
-  Assume we have a text file called "webdictionary.txt", stored on the server, that looks like this:
-  AJAX = Asynchronous JavaScript and XML
-  CSS = Cascading Style Sheets
-  HTML = Hyper Text Markup Language
-  PHP = PHP Hypertext Preprocessor
-  SQL = Structured Query Language
-  SVG = Scalable Vector Graphics
-  XML = EXtensible Markup Language
+  the fopen() is also used to create a file as well as open a file
+  example  -  $myfile = fopen("testfile.txt", "w");
+  wills create a file if it doesn't exist if the file is opened for writting("w")/appending("a")
+
+  The fwrite() function is used to write to a file.
+  The first parameter of fwrite() contains the name of the file to write to and
+  the second parameter is the string to be written.
+  example -  $myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
+             $txt = "John Doe\n";
+             fwrite($myfile, $txt);
+             $txt = "Jane Doe\n";
+             fwrite($myfile, $txt);
+             fclose($myfile);
+  Notice that we wrote to the file "newfile.txt" twice.
+  Each time we wrote to the file we sent the string $txt that first contained "John Doe" and second contained "Jane Doe".
+  After we finished writing, we closed the file using the fclose() function.
+
+  using "w" mode & overwritting deletes all previous existing data 
+  append "a" mode appends some data to the existing file
 -->
 
 <!DOCTYPE html>
@@ -40,40 +50,10 @@ fclose($myFile);
 </body>
 </html>
 <!-- 
-The first parameter of fopen() contains the name of the file to be opened
-  and the second parameter specifies in which mode the file should be opened.
 -->
 
 <!-- 
-  Modes: 
-        r = Open a file for read only. File pointer starts at the beginning of the file
-        w = Open a file for write only.
-            Erases the contents of the file or creates a new file if it doesn't exist. 
-            File pointer starts at the beginning of the file
-        a = Open a file for write only.
-            The existing data in file is preserved.
-            File pointer starts at the end of the file.
-            Creates a new file if the file doesn't exist
-        x = Creates a new file for write only. Returns FALSE and an error if file already exists
-        r+ = Open a file for read/write. File pointer starts at the beginning of the file
-        w+ = Open a file for read/write.
-             Erases the contents of the file or creates a new file if it doesn't exist.
-             File pointer starts at the beginning of the file
-        a+ = Open a file for read/write.
-             The existing data in file is preserved.
-             File pointer starts at the end of the file.
-             Creates a new file if the file doesn't exist
-        x+ = Creates a new file for read/write. Returns FALSE and an error if file already exists
 -->
 
 <!-- 
-  functions: 
-  fopen() - opens up a file
-  fread() - reads from a file, The first parameter of fread() contains the name of the file to read from
-            and the second parameter specifies the maximum number of bytes to read.
-  fclose() - closes an opened up file
-  fgets()  - used to read a single line from a file {Note: After a call to the fgets() function, the file pointer has moved to the next line}
-  feof() - checks if the "end-of-file" (EOF) has been reached. function is useful for looping through data of unknown length.
-           reads document file line by line until the end is reached
-  fgetc() - used to read a single character from a file {Note: After a call to the fgetc() function, the file pointer moves to the next character}
 -->
