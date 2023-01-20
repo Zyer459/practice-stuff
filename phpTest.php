a193539e4b43264f2503bd9d086dc21f7bd029a0
Zyer459 <asiful.sumit@gmail.com>
2023-01-20 12:41:54 +0600

    php filters

--- phpTest.php
+++ phpTest.php
@@ -1,14 +1,12 @@
 <!--  
-PHP ADVANCED: PHP SESSIONS -> a session is a way to store info as variables to be used across multiple pages
-              unlike a cookie info is not stored in user's computer.
-              the php session allows the server to remember the user across multiple pages
-              by default session variables last until the user closes the browser
+PHP ADVANCED: PHP FILTER -> filters are used to validate & sanitize external input
+                           such as-> User input from a form, cookies, web services data, server variables, database query results
 -->
 
 <!-- 
-  START A PHP SESSION: start a session with startsession() func
-    1) create a new page called demo_session1.php here start a new php session and set some session variables
-    2) Next, we create another page called "demo_session2.php". From this page, we will access the session information
+  the filter_var() function can both validate & sanitize data. It has 2 parameters 
+  1) the variable you want to check
+  2) the type of check to use
 -->
 
 <!--
@@ -23,23 +21,14 @@ PHP ADVANCED: PHP SESSIONS -> a session is a way to store info as variables to b
     <title>php document</title>
 </head>
 <body>
-  <?php
-  include "demo_session2.php";
-  print_r($_SESSION); // Another way to show all the session variable values for a user session is to run the following code
-  ?>
 </body>
 </html>
 
 <!-- 
-  Note: The session_start() function must be the very first thing in your document. Before any HTML tags.
 -->
 
 <!-- 
-  Notice that session variables are not passed individually to each new page,
-  instead they are retrieved from the session we open at the beginning of each page (session_start()).
-  Also notice that all session variable values are stored in the global $_SESSION variable
 -->
 
 <!-- 
-  Important! use include or require to use the session files
 -->
