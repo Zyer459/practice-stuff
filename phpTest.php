b05f6af61f22bea5ef6b68bcfec13223d142b9c7
Zyer459 <asiful.sumit@gmail.com>
2023-01-20 17:42:06 +0600

    php JSON-pt1

--- phpTest.php
+++ phpTest.php
@@ -1,16 +1,13 @@
 <!--  
-PHP ADVANCED: PHP CALLBACK -> A callback function (often referred to as just "callback")
-    is a function which is passed as an argument into another function.
-    Any existing function can be used as a callback function.
-    To use a function as a callback function,
-    pass a string containing the name of the function as the argument of another function
+PHP ADVANCED: PHP JSON -> JSON stands for JavaScript Object Notation, and is a syntax for storing and exchanging data.
+              PHP has some built-in functions to handle JSON.
+              First, we will look at the following two functions:
+              json_encode()
+              json_decode()
 -->
 
 <!-- 
-  Callbacks in User Defined Functions
-     User-defined functions and methods can also take callback functions as arguments.
-     To use callback functions inside a user-defined function or method,
-     call it by adding parentheses to the variable and pass arguments as with normal functions
+  json_encode() is used to a value to JSON format
 -->
 
 <!--
@@ -25,39 +22,39 @@ PHP ADVANCED: PHP CALLBACK -> A callback function (often referred to as just "ca
 </head>
 <body>
 <?php
-/** Example: Run a callback from a user-defined function*/
+/** Example: This example shows how to encode an associative array into a JSON object*/
 
-function exclaim($str) {
-  return $str . "! ";
-}
+$age = array ("Peter" => 35, "Ben" => 37, "Joe" => 41);
+echo json_encode($age);
 
-function ask($str) {
-  return $str . "? ";
-}
+echo "<br>";
+echo "<br>";
 
-function printFormatted($str, $format) {
-  // Calling the $format callback function
-  echo $format($str);
-}
+$cars = array ("Audi", "BMW", "Toyota"); // this example encodes an indexed array
+echo json_encode($cars);
 
-// Pass "exclaim" and "ask" as callback functions to printFormatted()
-printFormatted("Hello world", "exclaim");
-printFormatted("Hello world", "ask");
+echo "<br>";
+echo "<br>";
 
+$json_obj = json_encode ($age); // set the json obj to a variable
+var_dump(json_decode($json_obj)); // decode and show the json obj
+
+/**The json_decode() function returns an object by default.
+ * The json_decode() function has a second parameter, and when set to true, JSON objects are decoded into associative arrays
+*/
+
+echo "<br>";
+echo "<br>";
+
+var_dump(json_decode($json_obj, true)); //this gives an associative array
 ?>
 </body>
 </html>
 
-<!-- 
-  unlike JS  in PHP a callback is passed in argument as a string name of the function 
+<!--  
 -->
 
 <!-- 
-  Starting with version 7, PHP can pass anonymous functions as callback functions
-  for example:
-    $strings = ["apple", "orange", "banana", "coconut"];
-$lengths = array_map( function($item) { return strlen($item); } , $strings);
-print_r($lengths);
 -->
 
 <!-- 
