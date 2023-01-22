50cb66092fc87aeb3a34c3414f78906d6c5237b5
Zyer459 <asiful.sumit@gmail.com>
2023-01-22 20:56:47 +0600

    define+use-> class & obj

--- phpTest.php
+++ phpTest.php
@@ -19,6 +19,15 @@ CLASS & OBJECTS: Classes and objects are the two main aspects of object-oriented
 -->
 
 <!--
+Define a Class: A class is defined by using the class keyword, followed by the name of the class and a pair of curly braces ({})
+All its properties and methods go inside the braces
+syntax // class ClassName { // codes go here}
+
+Define Objects: Classes are nothing without objects! We can create multiple objects from a class
+Each object has all the properties and methods defined in the class, but they will have different property values.
+
+Objects of a class are created using the new keyword.
+In the example below, $apple and $banana are instances of the class Fruit
 -->
 
 <!DOCTYPE html>
@@ -31,6 +40,50 @@ CLASS & OBJECTS: Classes and objects are the two main aspects of object-oriented
 </head>
 <body>
 <?php
+/**Example: below we declare a class named Fruit consisting of two properties ($name and $color) and two methods
+ * set_name() and get_name() for setting and getting the $name property
+ */
+
+ class Fruit {
+  //properties = variables + value
+  public $name;
+  public $color;
+
+  //methods = functions
+  function set_name ($name) {
+    $this -> name = $name;
+  }
+  function get_name () {
+    return $this -> name;
+  }
+  //methods for setting & getting color
+  function set_color ($color) {
+    $this -> color = $color;
+  }
+  function get_color () {
+    return $this -> color;
+  }
+ }
+
+ // Define objects
+ $apple = new Fruit();
+ $banana = new Fruit();
+ //using set name method
+ $apple->set_name('Apple');
+ $banana->set_name('Banana');
+// using set name method
+ echo $apple->get_name();
+ echo "<br>";
+ echo $banana->get_name();
+
+ echo "<br>";
+ // setting & getting color
+ $apple -> set_color ("Red");
+ $banana -> set_color ("Yellow");
+ 
+ echo $apple -> get_color();
+ echo "<br>";
+ echo $banana -> get_color();
 
 ?>
 </body>
