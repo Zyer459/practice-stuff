f26d5d7292233c7cdc1d68e5cd538c3e2f1f4499
Zyer459 <asiful.sumit@gmail.com>
2023-01-22 20:27:26 +0600

    php OOP

--- phpTest.php
+++ phpTest.php
@@ -1,8 +1,21 @@
-<!--  
+<!-- 
+PHP OOP:
+       Procedural programming is about writing procedures or functions that perform operations on the data,
+       object-oriented programming is about creating objects that contain both data and functions.
+
+       Object-oriented programming has several advantages over procedural programming:
+       faster and easier to execute
+       provides a clear structure for the programs
+        helps to keep the PHP code DRY "Don't Repeat Yourself", and makes the code easier to maintain, modify and debug
+        makes it possible to create full reusable applications with less code and shorter development time
+        
 -->
 
 <!-- 
-
+CLASS & OBJECTS: Classes and objects are the two main aspects of object-oriented programming.
+ a class is a template for objects, and an object is an instance of a class.
+ When the individual objects are created, they inherit all the properties and behaviors from the class,
+ but each object will have different values for the properties.
 -->
 
 <!--
