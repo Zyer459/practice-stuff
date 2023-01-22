185f10b1c6ef6896071a47a78570e4570f705ac6
Zyer459 <asiful.sumit@gmail.com>
2023-01-22 20:05:56 +0600

    clear

--- phpTest.php
+++ phpTest.php
@@ -1,18 +1,11 @@
 <!--  
-PHP ADVANCED: PHP JSON -> JSON stands for JavaScript Object Notation, and is a syntax for storing and exchanging data.
-              PHP has some built-in functions to handle JSON.
-              First, we will look at the following two functions:
-              json_encode()
-              json_decode()
 -->
 
 <!-- 
-  json_encode() is used to encode a value to JSON format
+
 -->
 
 <!--
-  PHP - Looping Through the Values
-  You can also loop through the values with a foreach() loop:
 -->
 
 <!DOCTYPE html>
@@ -25,27 +18,6 @@ PHP ADVANCED: PHP JSON -> JSON stands for JavaScript Object Notation, and is a s
 </head>
 <body>
 <?php
-/** Example: This example shows how to loop through the values of a PHP object */
-
-$age = array ("Peter" => 35, "Ben" => 37, "Joe" => 41);
-$json_obj = json_encode ($age); // encode & set the json obj to a variable
-$obj = json_decode($json_obj); // decodes as obj
-
-foreach($obj as $key => $value) {
-  echo $key . " => " . $value . "<br>";
-}
-
-echo "<br>";
-echo "<br>";
-echo "<br>";
-
-/**This example shows how to loop through the values of a PHP associative array: */
-
-$arr = json_decode($json_obj, true); // decodes as associative array
-
-foreach($arr as $key => $value) {
-  echo $key . " => " . $value . "<br>";
-}
 
 ?>
 </body>
