bd381488153e0f1e476757dcebdecb5dcdaa8750
Zyer459 <asiful.sumit@gmail.com>
2023-01-24 18:26:13 +0600

    php constructor

--- phpTest.php
+++ phpTest.php
@@ -1,33 +1,14 @@
-<!-- 
-PHP OOP:
-       Procedural programming is about writing procedures or functions that perform operations on the data,
-       object-oriented programming is about creating objects that contain both data and functions.
-
-       Object-oriented programming has several advantages over procedural programming:
-       faster and easier to execute
-       provides a clear structure for the programs
-        helps to keep the PHP code DRY "Don't Repeat Yourself", and makes the code easier to maintain, modify and debug
-        makes it possible to create full reusable applications with less code and shorter development time
-        
+<!-- PHP OOP - Constructor
+               A constructor allows you to initialize an object's properties upon creation of the object
+               If you create a __construct() function, PHP will automatically call this function when you create an object from a class.
+               Notice that the construct function starts with two underscores (__)!
+               that using a constructor saves us from calling the set_name() method which reduces the amount of code      
 -->
 
 <!-- 
-CLASS & OBJECTS: Classes and objects are the two main aspects of object-oriented programming.
- a class is a template for objects, and an object is an instance of a class.
- When the individual objects are created, they inherit all the properties and behaviors from the class,
- but each object will have different values for the properties.
 -->
 
 <!--
-Define a Class: A class is defined by using the class keyword, followed by the name of the class and a pair of curly braces ({})
-All its properties and methods go inside the braces
-syntax // class ClassName { // codes go here}
-
-Define Objects: Classes are nothing without objects! We can create multiple objects from a class
-Each object has all the properties and methods defined in the class, but they will have different property values.
-
-Objects of a class are created using the new keyword.
-In the example below, $apple and $banana are instances of the class Fruit
 -->
 
 <!DOCTYPE html>
@@ -40,69 +21,56 @@ In the example below, $apple and $banana are instances of the class Fruit
 </head>
 <body>
 <?php
-/**Example: below we declare a class named Fruit consisting of two properties ($name and $color) and two methods
- * set_name() and get_name() for setting and getting the $name property
- */
+/**Example: the construtor reduces code and saves time*/
 
- class Fruit {
-  //properties = variables + value
-  public $name;
-  public $color;
-
-  //methods = functions
-  function set_name ($name) {
-    $this -> name = $name;
+class Fruit {
+    public $name;
+    public $color;
+  
+    function __construct($name) {
+      $this->name = $name;
+    }
+    function get_name() {
+      return $this->name;
+    }
   }
-  function get_name () {
-    return $this -> name;
-  }
-  //methods for setting & getting color
-  function set_color ($color) {
-    $this -> color = $color;
-  }
-  function get_color () {
-    return $this -> color;
-  }
- }
+  
+  $apple = new Fruit("Apple");
+  echo $apple->get_name();
 
- // Define objects
- $apple = new Fruit();
- $banana = new Fruit();
- //using set name method
- $apple->set_name('Apple');
- $banana->set_name('Banana');
-// using set name method
- echo $apple->get_name();
- echo "<br>";
- echo $banana->get_name();
+  echo "<br>";
+  echo "<br>";
+  echo "<br>";
+  echo "<br>";
 
- echo "<br>";
- // setting & getting color
- $apple -> set_color ("Red");
- $banana -> set_color ("Yellow");
- 
- echo $apple -> get_color();
- echo "<br>";
- echo $banana -> get_color();
-
+  class Fruit2 {
+    public $name;
+    public $color;
+  
+    function __construct($name, $color) {
+      $this->name = $name;
+      $this->color = $color;
+    }
+    function get_name() {
+      return $this->name;
+    }
+    function get_color() {
+      return $this->color;
+    }
+  }
+  
+  $apple = new Fruit2("Apple", "red");
+  echo $apple->get_name();
+  echo "<br>";
+  echo $apple->get_color();
 ?>
 </body>
 </html>
 
 <!--  
-  PHP - instanceof:
-                  You can use the instanceof keyword to check if an object belongs to a specific class
-                  Example:
-                  $apple = new Fruit();
-                  var_dump($apple instanceof Fruit);
 -->
 
 <!-- 
-  PHP - The $this Keyword
-  The $this keyword refers to the current object, and is only available inside methods.
-  there are 2 ways to change properties in class/objects
-  1) Inside the class (by adding a set_name() method and use $this)
-  2) Outside the class (by directly changing the property value)
 -->
 
 <!-- 
