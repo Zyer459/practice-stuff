952d9694a9ef337a9970b830a4bc57d33f97b61a
Zyer459 <asiful.sumit@gmail.com>
2023-01-25 21:25:15 +0600

    php-overriding protected access modifier

--- phpTest.php
+++ phpTest.php
@@ -12,6 +12,10 @@
 -->
 
 <!--
+  PHP - Overriding Inherited Methods:
+       Inherited methods can be overridden by redefining the methods (use the same name) in the child class.
+       Look at the example below. The __construct() and intro() methods in the child class (Strawberry) will override the
+       __construct() and intro() methods in the parent class (Fruit)
 -->
 
 <!DOCTYPE html>
@@ -24,9 +28,7 @@
 </head>
 <body>
 <?php
-/**Example: protected access modifiers
- * In the example above we see that all works fine!
- * It is because we call the protected method (intro()) from inside the derived class.
+/**Example: overriding protected access modifiers
 */
 
 class Fruit {
@@ -38,19 +40,23 @@ class Fruit {
     }
     protected function intro() {
       echo "The fruit is {$this->name} and the color is {$this->color}.";
-    } // changed the access modifier from public to protected
-  }
-  
-  // Strawberry is inherited from Fruit
-  class Strawberry extends Fruit {
-    public function message() {
-      echo "Am I a fruit or a berry? ";
-      // calling the method from inside the derived class - OK
-      $this->intro();
     }
   }
-  $strawberry = new Strawberry("Strawberry", "red"); // OK.  __construct() is public
-  $strawberry->message();  // OK. message() is public and it calls intro() (which is protected) from within the derived class
+
+  class Strawberry extends Fruit {
+    public $weight;
+    public function __construct($name, $color, $weight) {
+      $this->name =$name;
+      $this->color =$color;
+      $this->weight =$weight;
+    }
+    public function intro () {
+      echo "The fruit is {$this->name}, the color is {$this->color}, and the weight is {$this->weight} gram.";
+    }
+  }
+
+  $strawberry = new Strawberry("Strawberry", "red", 50);
+  $strawberry->intro();
 
 ?>
 </body>
