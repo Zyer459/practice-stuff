a01b5503951800b01bad3639aacaab64a44b8abf
Zyer459 <asiful.sumit@gmail.com>
2023-01-26 19:36:25 +0600

    php traits-example explained

--- phpTest.php
+++ phpTest.php
@@ -54,6 +54,14 @@ trait message1 {
 </html>
 
 <!-- 
+  Example Explained:
+                    Here, we declare one trait: message1.
+                    Then, we create a class: Welcome.
+                    The class uses the trait, and all the methods in the trait will be available in the class.
+                    If other classes need to use the msg1() function, simply use the message1 trait in those classes.
+                    This reduces code duplication, because there is no need to redeclare the same method over and over again.
+
+
 -->
 
 <!-- 
