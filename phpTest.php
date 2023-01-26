abf2729ee73afbeea2a2e04448ed7f98c5ae978a
Zyer459 <asiful.sumit@gmail.com>
2023-01-26 18:25:06 +0600

    php interfaces-pt2

--- phpTest.php
+++ phpTest.php
@@ -38,29 +38,52 @@
  * A class that implements an interface must implement all of the interface's methods.
 */
 
+// Interface definition
 interface Animal {
   public function makeSound();
 }
 
+// Class definitions
 class Cat implements Animal {
   public function makeSound() {
-    echo "Meow";
+    echo " Meow ";
   }
 }
 
-$animal = new Cat();
-$animal->makeSound();
+class Dog implements Animal {
+  public function makeSound() {
+    echo " Bark ";
+  }
+}
+
+class Mouse implements Animal {
+  public function makeSound() {
+    echo " Squeak ";
+  }
+}
+
+// Create a list of animals
+$cat = new Cat();
+$dog = new Dog();
+$mouse = new Mouse();
+$animals = array($cat, $dog, $mouse);
+
+// Tell the animals to make a sound
+foreach($animals as $animal) {
+  $animal->makeSound();
+}
 
 ?>
 </body>
 </html>
 
 <!-- 
-  From the example above, let's say that we would like to write software which manages a group of animals.
-  There are actions that all of the animals can do, but each animal does it in its own way.
-  Using interfaces, we can write some code which can work for all of the animals even if each animal behaves differently
-  
-  see php interfaces-pt2
+Example Explained:
+                Cat, Dog and Mouse are all classes that implement the Animal interface,
+                which means that all of them are able to make a sound using the makeSound() method
+                Because of this, we can loop through all of the animals and tell them to make a sound even if we don't know what type of animal each one is.
+                Since the interface does not tell the classes how to implement the method,
+                each animal can make a sound in its own way.
 -->
 
 <!-- 
